1046569525989363aceec73ec9dd31b05d55f0f6
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2026-04-20 00:40:54 +0300

    add to Comment

---
+++ InsureYouAI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsureYouAI.Areas.Admin.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly InsureContext _context;
+
+        public CommentController(InsureContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> CommentList()
+        {
+            var values = _context.Comments.Include(x=>x.Article).Include(x=>x.AppUser).ToList();
+            return View(values);
+        }
+    }
+}
